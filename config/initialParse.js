dd6e3a707256eda12f74c883677ea7a16743aedb
aldorodrigorivera <69479743+aldorodrigorivera@users.noreply.github.com>
2021-04-28 15:41:01 -0500

    Update initialParse.js

--- config/initialParse.js
+++ config/initialParse.js
@@ -2,9 +2,9 @@ const Parse = require('parse/node');
 
 module.exports = {
     initializeParse(){
-        Parse.initialize('kU7rMeE8oYGPJJPixUBBFWNIikXJpMip6VjnaAuT','iIgzaF9z8J2pjiUQTremh7Utrayvc5LJgnxYoT7q');
+        Parse.initialize('ID','JAVASCRIPT');
         Parse.serverURL = "https://parseapi.back4app.com/";
-        Parse.masterKey = "CCNgxRdknduiXI2T26FjwiE70RyJsVV5eueQBfM0";
+        Parse.masterKey = "MASTER";
         return Parse;
     },
     UserClass: Parse.Object.extend("User"),
